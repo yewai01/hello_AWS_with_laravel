b5e664e410a0971534aef85dac92431fcdffdc41
Ye Wai <scm.yewai@gmail.com>
2022-10-17 16:51:12 +0630

    change

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -41,7 +41,7 @@
                     <p style="
                         font-size: 40px;
                         color: red;
-                    ">Testing github actions and codeDeploy...</p>
+                    ">Testing github actions and codeDeploy... It work???</p>
                 </div>
 
                 <div class="mt-8 bg-white dark:bg-gray-800 overflow-hidden shadow sm:rounded-lg">
