03a57d700611ddcdef041c70e8d6bbcf80366ede
Ye Wai <scm.yewai@gmail.com>
2022-10-18 09:53:28 +0630

    final test

--- deploy/scripts/change_permissions.sh
+++ deploy/scripts/change_permissions.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-sh "sudo -i -u ec2-user composer install --working-dir=/var/www/html/hello_AWS_with_laravel"
+sudo -i -u ec2-user composer install --working-dir=/var/www/html/hello_AWS_with_laravel
