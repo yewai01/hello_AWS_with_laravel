0870d6f3644b4444bb57108cd8c65325c4ae447c
Ye Wai <scm.yewai@gmail.com>
2022-10-18 11:11:18 +0630

    develop

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -41,7 +41,7 @@
                     <p style="
                         font-size: 40px;
                         color: red;
-                    ">Testing github actions and codeDeploy... finally work -_-</p>
+                    ">Testing github actions and codeDeploy... finally work develop branch</p>
                 </div>
 
                 <div class="mt-8 bg-white dark:bg-gray-800 overflow-hidden shadow sm:rounded-lg">
